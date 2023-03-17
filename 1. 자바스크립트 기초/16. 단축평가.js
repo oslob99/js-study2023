7c6fff99f2e0c247a1c46bfe2fe23930be25c326
oslob99 <jkh991116@naver.com>
2023-03-17 10:27:11 +0900

    js 연산자 단축

---
+++ 1. 자바스크립트 기초/16. 단축평가.js
@@ -0,0 +1,22 @@
+
+
+// OR 연산 : 첫번째 truthy 반환
+
+console.log('hello' || 'world');
+console.log(0 || '안녕');
+
+// AND 연산 : 첫번째 falsy 반환
+
+console.log('============');
+console.log('HELLO' && 'WORLD');
+console.log(null && '메롱');
+
+/*
+
+    <h1> 하이루 환영 </h1>
+    isLogin() && <h2>xxx님 하이루!</h2>
+
+    쿠폰당첨플래그 && sendCoupon()
+
+    isMineArticle() && <button>수정</button>
+*/
