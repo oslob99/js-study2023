5df4ef170c6973175ef68c154649e68238244b87
oslob99 <jkh991116@naver.com>
2023-03-21 10:40:08 +0900

    let과 const 쓰기!

---
+++ 4. 함수/11. const.js
@@ -0,0 +1,18 @@
+
+const TODAY_SALE_RATE = 0.2;
+
+const PI = 3.14159265;
+
+// PI = 4.4; xxxxx
+
+let kim = {
+    name: '김철수',
+    age: 25
+};
+
+kim.name = '김칠리';
+
+kim = {
+    name: '김철수',
+    age: 25
+};
